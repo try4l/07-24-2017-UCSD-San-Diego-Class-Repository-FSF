56dbdb3de9118ab1b5acfe7873e484970cad66e5
Matthew Valli <matthew.valli@gmail.com>
2017-10-16 21:32:12 -0700

    Added Week 12 Activities

---
+++ Week 12/Activities/11-popQuiz/Solved/colleges.js
@@ -0,0 +1,30 @@
+var mysql = require("mysql");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "boston"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  readColleges();
+});
+
+function readColleges() {
+  connection.query("SELECT name FROM colleges", function(err, res) {
+    if (err) throw err;
+
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
